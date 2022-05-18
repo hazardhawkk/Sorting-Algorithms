b8560b4f8825d7113b80dbf24f14033b83ec1734
Shahzaib Zafar <21235452@shrewsbury.ac.uk>
2022-05-18 10:34:12 +0100

    init

--- Sorting Algorithms/Program.cs
+++ Sorting Algorithms/Program.cs
@@ -10,7 +10,7 @@ namespace Sorting_Algorithms
 
             
 
-            BubbleSort(arrOfIntegers);
+            OptimizedBubbleSort(arrOfIntegers);
 
 
 
@@ -22,16 +22,17 @@ namespace Sorting_Algorithms
                 Console.Write( arr[i]+", ");
             }
         }
+
         static void BubbleSort(int[] arr)
         {
             Console.WriteLine("Unsorted Array:");       //writing unsorted
             WriteArray(arr);                    
             Console.WriteLine();
 
-            for (int i = 0;i<arr.Length;i++)          //itterating through passes
+            for (int i = 0;i<arr.Length-2;i++)          //itterating through passes
                 {
 
-                  for (int j = 0; j < arr.Length-1; j++)     //itterating through items
+                  for (int j = 0; j < arr.Length-2; j++)     //itterating through items
                   {
 
 
@@ -48,7 +49,33 @@ namespace Sorting_Algorithms
                 }
             Console.WriteLine("Sorted Array:");       //writing sorted
             WriteArray(arr); 
-            
+        }
+
+        static void OptimizedBubbleSort(int[] arr)
+        {
+            bool swaps_made = true;
+            int i=0    //counting through while loop
+            int passes_made = 1;
+
+            Console.WriteLine("Unsorted Array:");       //writing unsorted
+            WriteArray(arr);                    
+            Console.WriteLine();
+
+            while (swaps_made==true || i<arr.Length-2)   //itterating through passes until no swaps are made in a pass or we have gone through the whole arr so are guaranteed to have sorted it
+            { 
+                  for (int j = 1; j < arr.Length - passes_made; j++)     //itterating through items for the length of the array excluding items at the end which are guaranteed to be in correct place
+                  {
+                    if (arr[j] > arr[j + 1])         //swapping if j bigger than j+1
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                  }
+                  i = i+1; 
+            }
+            Console.WriteLine("Sorted Array:");       //writing sorted
+            WriteArray(arr); 
         }
     }
 }
