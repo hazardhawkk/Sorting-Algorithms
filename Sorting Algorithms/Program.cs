29be8f06e3c3b72bd17ae6250f0e80a89479f4bf
hazardhawkk <shahzaibzafarshazi@gmail.com>
2022-05-23 11:00:20 +0100

    done bubble and insertion

--- Sorting Algorithms/Program.cs
+++ Sorting Algorithms/Program.cs
@@ -8,8 +8,8 @@ namespace Sorting_Algorithms
         {
             int[] arrOfIntegers = {2,8,4,1,8,3,2,63,7,2,5};
 
-            
 
+           
             InsertionSort(arrOfIntegers);
 
 
@@ -30,16 +30,18 @@ namespace Sorting_Algorithms
             WriteArray(arr);                    
             Console.WriteLine();
 
-                for (int i = 0;i<arr.Length-2;i++)          //itterating through passes
+            int temp = 0;
+
+                for (int i = 0;i<arr.Length-1;i++)          //itterating through passes
                 {
 
-                  for (int j = 0; j < arr.Length-2; j++)     //itterating through items
+                  for (int j = 0; j < arr.Length-1; j++)     //itterating through items
                   {
 
 
                     if (arr[j] > arr[j + 1])         //swapping if j bigger than j+1
                     {
-                        int temp = arr[j];
+                        temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
                     }
@@ -54,17 +56,16 @@ namespace Sorting_Algorithms
         static void OptimizedBubbleSortWithForLoop(int[] arr)
         {
             bool swaps_made = true;
-            int passes_made = 1;
 
             Console.WriteLine("Unsorted Array:");       //writing unsorted
             WriteArray(arr);                    
             Console.WriteLine();
 
-                for (int i = 0;i<arr.Length-2 || swaps_made==true;i++)          //itterating through passes
-                {
+                for (int i = 0;i<arr.Length-1 || swaps_made==true;i++)          //itterating through passes until no swaps are made in a pass or we have gone through the whole arr so are guaranteed to have sorted it
+            {
 
-                  for (int j = 1; j < arr.Length - passes_made; j++)     //itterating through items
-                  {
+                  for (int j = 0; j < arr.Length - (1+i); j++)     //itterating through items for the length of the array excluding items at the end which are guaranteed to be in correct place
+                {
                     swaps_made=false;
 
                     if (arr[j] > arr[j + 1])         //swapping if j bigger than j+1
@@ -86,16 +87,14 @@ namespace Sorting_Algorithms
         static void OptimizedBubbleSortWithWhileLoop(int[] arr)
         {
             bool swaps_made = true;
-            int i=0    //counting through while loop
-            int passes_made = 1;
-
+            int i = 0;
             Console.WriteLine("Unsorted Array:");       //writing unsorted
             WriteArray(arr);                    
             Console.WriteLine();
 
-            while (swaps_made==true || i<arr.Length-2)   //itterating through passes until no swaps are made in a pass or we have gone through the whole arr so are guaranteed to have sorted it
+            while (swaps_made==true || i < arr.Length-1)   //itterating through passes until no swaps are made in a pass or we have gone through the whole arr so are guaranteed to have sorted it
             { 
-                  for (int j = 1; j < arr.Length - passes_made; j++)     //itterating through items for the length of the array excluding items at the end which are guaranteed to be in correct place
+                  for (int j = 0; j < arr.Length - (1+i); j++)     //itterating through items for the length of the array excluding items at the end which are guaranteed to be in correct place
                   {
                      swaps_made=false;
                     if (arr[j] > arr[j + 1])         //swapping if j bigger than j+1
@@ -106,7 +105,8 @@ namespace Sorting_Algorithms
                       swaps_made=true;
                     }
                   }
-                  i++; 
+                i++;
+                  
             }
             Console.WriteLine("Sorted Array:");       //writing sorted
             WriteArray(arr); 
@@ -118,22 +118,23 @@ namespace Sorting_Algorithms
             WriteArray(arr);                    
             Console.WriteLine();
 
-
+            int currentItem = 0;
+            int previousIndex = 0;
 
             
 
-            for (int i = 1; i < arr.Length-1; i++)          //itterating through arr starting at second item (index 1)
+            for (int i = 1; i < arr.Length; i++)          //itterating through arr starting at second item (index 1)
 			{
-                int currentItem=i;    //<-- variable created to not lose the value of current item
-              int previousIndex = i-1;
+               currentItem= arr[i];    //<-- variable created to not lose the value of current item
+               previousIndex = i-1;
 
-                while (arr[previousIndex] > arr[currentItem])  //iterating through inserting each item in correct place
+                while (previousIndex >= 0 && arr[previousIndex] > currentItem)  //iterating through inserting each previous item in correct place by moving it up the array
                 {
                     arr[previousIndex+1] = arr[previousIndex];
 
                     previousIndex = previousIndex-1;
                 }
-                arr[previousIndex+1] = arr[currentItem];        //+1 bc we have checked one behind the correct position
+                arr[previousIndex+1] = currentItem;        //+1 bc we have checked one behind the correct position
 	
 			}
 
